cfe1a8cce7236598422c2fb9b5bbf6de1278281b
eligantRU <eligant.ru@gmail.com>
2016-08-16 23:26:48 +0400

    Refactoring
    Signed-off-by: eligantRU <eligant.ru@gmail.com>

--- jumping_balls/jumping_balls/b2Object.h
+++ jumping_balls/jumping_balls/b2Object.h
@@ -8,15 +8,15 @@ public:
 	Cb2Object() = default;
 	~Cb2Object() = default;
 
-	void SetPosition(glm::vec2 position) { };
+	void SetPosition(glm::vec2 position) { (void)position; };
 
 	void SetSize() { };
 
-	void SetData(unsigned id) { };
+	void SetData(unsigned id) { (void)id; };
 
 	unsigned GetData() const { };
 
-	void ApplyImpulse(glm::vec2 direction) { };
+	void ApplyImpulse(glm::vec2 direction) { (void)direction; };
 
 private:
 };
