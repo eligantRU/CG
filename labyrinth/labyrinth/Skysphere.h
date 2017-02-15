f08071e1469f4a422ec4428ca37f3e0cb0739b15
eligantRU <eligant.ru@gmail.com>
2017-02-16 01:25:56 +0400

    Added forgotten file

--- labyrinth/labyrinth/Skysphere.h
+++ labyrinth/labyrinth/Skysphere.h
@@ -8,10 +8,10 @@ class CSkysphere final
 public:
 	CSkysphere();
 	void Update(const float dt);
-	void Draw() const;
+	void Draw(IRenderer3D & renderer) const;
 
 private:
 	CTexture2DUniquePtr m_pSkyTexture;
-	std::unique_ptr<CIdentitySphere> m_sphere;
+	CIdentitySphere m_sphere;
 	float m_animationPhase = 0;
 };
